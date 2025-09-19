da61295bd50494b5d6c08a158bac5b4be3233fa6
Awin Gray <grayawin@gmail.com>
2025-09-19 21:29:13 +0700

    add migrations for eventFlow db

--- EventFlow.Api/Program.cs
+++ EventFlow.Api/Program.cs
@@ -1,4 +1,6 @@
+using EventFlow.Infrastructure.Data.Contexts;
 using EventFlow.Infrastructure.DependencyInjections;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EventFlowWriteContext>();
+    await db.Database.MigrateAsync();
 }
 
 app.UseHttpsRedirection();
