b95bd32922cb968439557b1cd317ee2a756fc741
Awin Gray <grayawin@gmail.com>
2025-10-02 18:28:13 +0700

    added migrations

---
+++ src/Znaphub.Infrastructure/Data/Migrations/Write/20251002112709_Unknown.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZnapHub.Infrastructure.Data.Migrations.Write
+{
+    /// <inheritdoc />
+    public partial class Unknown : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Photos");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Photos",
+                type: "character varying(2048)",
+                maxLength: 2048,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
