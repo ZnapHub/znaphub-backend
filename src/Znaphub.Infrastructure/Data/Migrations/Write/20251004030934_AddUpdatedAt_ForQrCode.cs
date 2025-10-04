318ae5a6f5b9b32ba0273571a3a8ef96c2f2ce2a
Awin Gray <grayawin@gmail.com>
2025-10-04 10:10:52 +0700

    add migration to add updatedAt for qrCode

---
+++ src/Znaphub.Infrastructure/Data/Migrations/Write/20251004030934_AddUpdatedAt_ForQrCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZnapHub.Infrastructure.Data.Migrations.Write
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAt_ForQrCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdatedAt",
+                table: "QrCodes",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "QrCodes");
+        }
+    }
+}
