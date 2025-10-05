62a12b8acab4018f9601972b2ff7488ebf9e6457
Awin Gray <grayawin@gmail.com>
2025-10-05 17:14:42 +0700

    remove minio from compose and add qrCode controller

---
+++ src/Znaphub.Api/Controllers/QrCodeController.cs
@@ -0,0 +1,39 @@
+using System.Net;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ZnapHub.Application.Abstractions.Messaging.Commands;
+using ZnapHub.Application.Features.QrCodes.Commands;
+using ZnapHub.Shared.Abstractions;
+
+namespace ZnapHub.Api.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("[controller]")]
+public class QrCodeController : ControllerBase
+{
+    private readonly ICommandDispatcher _commandDispatcher;
+
+    public QrCodeController(ICommandDispatcher commandDispatcher)
+    {
+        _commandDispatcher = commandDispatcher;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(
+        [FromBody] GenerateQrCodeCommand command,
+        CancellationToken ct
+    )
+    {
+        var result = await _commandDispatcher.DispatchAsync(command, ct);
+        return result.Match<IActionResult>(
+            Ok,
+            e =>
+                e switch
+                {
+                    not null when e == Error.NotFound => BadRequest(e),
+                    _ => StatusCode((int)HttpStatusCode.InternalServerError, e),
+                }
+        );
+    }
+}
